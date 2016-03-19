d06c0cd6ed462f32dd399ea7c01236ebc24dc6ff
Alex Dutton <alex.dutton@duttonsoftware.com>
2016-03-19 10:24:19 -0700

    added leap year function and cleared up main

--- src/Geometry/Main.java
+++ src/Geometry/Main.java
@@ -1,27 +1,9 @@
 package Geometry;
-
 import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		//System.out.println ("Hello World");
-		//Scanner input = new Scanner( System.in );
-		//String s = input.next();
-		//System.out.println(s);
-		//float x = Float.parseFloat(s);
-		//Point p = (x,2.965);
-		//System.out.println (p.toString());
-		Point p1 = Point.Origin;
-		Point p2 = new Point(10, 10);
-		LineSegment ls = new LineSegment(p1, p2);
 		
-		System.out.println (ls.midpoint());
-		
-		
-		System.out.println(Points.distance(new Point(1,6), new Point (2,5)));
-		
+		System.out.println(MathX.leapyear(100));
 		
 	}
 
--- src/Geometry/MathX.java
+++ src/Geometry/MathX.java
@@ -1,7 +1,25 @@
 package Geometry;
-
 public class MathX {
 	public static double square(double x){
 		return x*x;
+	}
+	public static boolean leapyear (int year){
+		if (year%4 == 0) {
+			 if  (year%100>0){
+				 return true;
+			 }
+			 else{
+				 if (year%400==0){
+					 return true;
+				 }
+				 
+				 else{
+					 return false;
+				 }
+			 }
+		}
+		else{
+			return false;
+		}
 	}
 }
